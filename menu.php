aaa2232b1682cd25a150fb9d6a284d4c08d56b3e
anabelvitaliani <48593669+anabelvitaliani@users.noreply.github.com>
2019-04-15 12:35:27 -0300

    Add files via upload
    Página de preguntas frecuentes y menú con el link

---
+++ menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="../Menu/menu.css" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC:700i" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<title>Chocolatemia</title>
+</head>
+<body>
+
+	<header>
+		<div class="logoymenu">
+	<!--<img src="../img/chocolate-tablet-with-bite.png" alt="" class="logo"> Negro sbre marron no se ve, y en mobile se superpone con el nombre-->
+  <h1>CHOCOLATEMIA</h1><img src="../img/menu2.png" alt="" class="menu"></div>
+	  <ul class="navegador">
+	  <li><a href="../Index/ecommerce.php">Inicio</a></li>
+	  <li><a href="../PreguntasFrecuentes/preguntas.php">Preguntas Frecuentes</a></li>
+	  <li><a href="../Login/login.php">Iniciar Sesion</a></li>
+	  <li><a href="../Registro/registro.php">Registro</a></li>
+		<li><a href="#contacto">Contacto</a></li>
+	</ul>
+	</header>
+
+  </body>
+  </html>
