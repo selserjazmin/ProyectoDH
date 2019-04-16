91e4fa3a2cb0f90d5a87609e0b88a5c4bd069103
selserjazmin <48593579+selserjazmin@users.noreply.github.com>
2019-04-15 21:16:50 -0300

    linkee el titulo del menú y saqué archivos duplicados

--- ProyectoIntegrador/PreguntasFrecuentes/preguntas.php
+++ ProyectoIntegrador/PreguntasFrecuentes/preguntas.php
@@ -1,11 +1,54 @@
 <!Doctype html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Restaurante-El buen sabor</title>
-    <link href="preguntas.css"
-    rel="stylesheet">
-  </head>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="../Index/ecommercestyle.css" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC:700i" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<title>Chocolatemia</title>
+</head>
   <body>
+    <?php
+    	require('../Menu/menu.php');
+    ?>
+    <main class='preguntas'>
+      <article>
+        <b>Hola! ¿Hacen envíos?</b>
+        <p>Hacemos envíos a CABA y Gran Buenos Aires.</p>
+      </article>
+      <article>
+        <b>¿Venden chocolates sin T.A.C.C?</b>
+        <p>¡Hola! Tenemos una sección apta para celíacos. Podés verla en nuestra página!</p>
+      </article>
+      <article>
+        <b>¿Qué gustos tienen?</b>
+        <p>Tenemos chocolate blaco, amargo, semiamargo, con frutos secos (nuez, maní, almendras o combinado), rellenos de dulce de leche, frambuesa, menta, ¡entre muchos otros!</p>
+      </article>
+      <article>
+        <b>¿Qué costo tienen los envíos?</b>
+        <p>Si es a una dirección de CABA el envío es sin costo. Si no, se suma un 5% al valor de la compra.</p>
+      </article>
+      <article>
+        <b>¿Cuáles son los puntos para retirar las compras?</b>
+        <p>Podés retirar tu compra en nuestros locales (Ugarte 2732, Belgrano o Quintino Bocayúa 1542, Boedo) de 8 a 20hs.</p>
+      </article>
+      <article>
+        <b>¿Hacen tortas y postres?</b>
+        <p>Hacemos reposteríá únicamente a pedido.</p>
+      </article>
+      <article>
+        <b>Con cuánta anticipación tengo que realizar el pedido?</b>
+        <p>Los pedidos se retiran 48hs hábiles después de realizados en caso de ser chocolates que estén en nuestra página. Para otros sabores, formas o repostería se requieren 96hs hábiles. </p>
+      </article>
+      <article>
+        <b>Puedo reservar una compra para retirarlo más tarde en el local?</b>
+        <p>No hacemos reservas, las compras son por orden de llegada. Si sabés qué vas a necesitar con anticipación podés hacer un pedido con 48hs hábiles de anticipación.</p>
+      </article>
+    </main>
+    <br>
+    <?php
+    include('../Footer/footer.php');
+    ?>
   </body>
   </html>
